0b2dd39d136635228d98e338a931e5d0db2424c2
devrazec <devrazec@gmail.com>
2026-02-08 09:25:07 +0000

    Update

--- src/app/lib/db/create_question.js
+++ src/app/lib/db/create_question.js
@@ -4,7 +4,7 @@ import db from "./connection.js";
 
 db.exec("PRAGMA foreign_keys = ON");
 
-const array_question_id = [1,2,3,4,6,7,8,9,10,11,13,14,16,17,19,20,21,23,24,26,27,28,31,32,37,38,39,41,42,43,44];
+const array_question_id = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53];
 
 for (const ques_id of array_question_id) {
 
